17971d818a0497b308bf8f571768b32a222134aa
Jairo Calderon <jairo_cal@yahoo.com>
2025-02-22 16:32:04 -0800

    fixing issue

--- utils/log_debugger/helper.js
+++ utils/log_debugger/helper.js
@@ -3,7 +3,6 @@ const vscode = require("vscode");
 function analyzeDebugLog() {
     // Display a message box to the user
     vscode.window.showInformationMessage("Initializing Log Analyzer!");
-    
     console.log("Will start the logic to analyze our debug log");
     
 }
