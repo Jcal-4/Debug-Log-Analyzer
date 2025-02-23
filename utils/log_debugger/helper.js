c88f4183da9fcc0604e5afd2f39c300641eda794
Jairo Calderon <jairo_cal@yahoo.com>
2025-02-22 17:48:56 -0800

    extension successfully reads open file when command is executed

--- utils/log_debugger/helper.js
+++ utils/log_debugger/helper.js
@@ -4,7 +4,28 @@ function analyzeDebugLog() {
     // Display a message box to the user
     vscode.window.showInformationMessage("Initializing Log Analyzer!");
     console.log("Will start the logic to analyze our debug log");
-    
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        console.log("no active editor found");
+        return;
+    } else {
+        // Get current file URI
+        const fileUri = editor.document.uri;
+        try {
+            const fileData = readFile(fileUri).then((fileData) => {
+                const fileContent = new TextDecoder().decode(fileData);
+                console.log("Decoded File Data: ", fileContent);
+                // return data;
+            });
+        } catch (error) {
+            // vscode.window.showErrorMessage(`Error reading file: ${error}`);
+            vscode.window.showInformationMessage(`Error reading file: ${error}`);
+        }
+    }
+}
+
+async function readFile(URI) {
+    return await vscode.workspace.fs.readFile(URI);
 }
 
 module.exports = analyzeDebugLog;
