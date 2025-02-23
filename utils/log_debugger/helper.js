d13658411184dc44401e90811191627fe6a33efd
Jcal-4 <74951540+Jcal-4@users.noreply.github.com>
2025-02-22 16:18:01 -0800

    Update helper.js

--- utils/log_debugger/helper.js
+++ utils/log_debugger/helper.js
@@ -3,6 +3,7 @@ const vscode = require("vscode");
 function analyzeDebugLog() {
     // Display a message box to the user
     vscode.window.showInformationMessage("Initializing Log Analyzer!");
+    
     console.log("Will start the logic to analyze our debug log");
     
 }
