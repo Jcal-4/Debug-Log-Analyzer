06d3317f0bdb2a471ecb52c30af8d2af3c1f6ff9
Jairo Calderon <jairo_cal@yahoo.com>
2025-02-22 21:03:28 -0800

    Successfully get all METHOD_ENTRY (minimal filtering)

--- utils/log_debugger/helper.js
+++ utils/log_debugger/helper.js
@@ -3,7 +3,6 @@ const vscode = require("vscode");
 function analyzeDebugLog() {
     // Display a message box to the user
     vscode.window.showInformationMessage("Initializing Log Analyzer!");
-    console.log("Will start the logic to analyze our debug log");
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
         console.log("no active editor found");
@@ -12,13 +11,20 @@ function analyzeDebugLog() {
         // Get current file URI
         const fileUri = editor.document.uri;
         try {
-            const fileData = readFile(fileUri).then((fileData) => {
+            readFile(fileUri).then((fileData) => {
                 const fileContent = new TextDecoder().decode(fileData);
-                console.log("Decoded File Data: ", fileContent);
+                let executedComponents = [];
+                executedComponents = retrieveComponents(fileContent);
+                if (executedComponents.length === 0) {
+                    vscode.window.showInformationMessage("No components found in the log file");
+                } else {
+                    for (let i = 0; i < executedComponents.length; i++) {
+                        console.log("executedComponent --> ", executedComponents[i]);
+                    }
+                }
                 // return data;
             });
         } catch (error) {
-            // vscode.window.showErrorMessage(`Error reading file: ${error}`);
             vscode.window.showInformationMessage(`Error reading file: ${error}`);
         }
     }
@@ -26,6 +32,32 @@ function analyzeDebugLog() {
 
 async function readFile(URI) {
     return await vscode.workspace.fs.readFile(URI);
+}
+
+function retrieveComponents(fileContent) {
+    const lines = fileContent.split("\n");
+    let executedComponents = [];
+    let ignoreList = ["system.", ".bulkBefore", ".bulkAfter", "Logger.", "Math.", "LoggerParameter.", "EncodingUtil.", "EventServices."];
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (line.includes("METHOD_ENTRY")) {
+            const parts = line.split("|");
+            const methodDetails = parts[parts.length - 1];
+            const methodDetailsLowercase = methodDetails.toLowerCase();
+            let shouldIgnore = false;
+            ignoreList.forEach((ignoreItem) => {
+                if (methodDetailsLowercase.includes(ignoreItem.toLowerCase())) {
+                    console.log("Ignoring method: ", methodDetailsLowercase, " due to ignore item: ", ignoreItem);
+                    shouldIgnore = true;
+                }
+            });
+            if (shouldIgnore) {
+                continue;
+            }
+            executedComponents.push(methodDetails);
+        }
+    }
+    return executedComponents;
 }
 
 module.exports = analyzeDebugLog;
