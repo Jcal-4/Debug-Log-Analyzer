60442b97765d53923f004e62b0e8b706b1d6b830
Jairo Calderon <jairo_cal@yahoo.com>
2025-02-26 21:13:09 -0800

    cleaned up folders still working react webview

--- utilssss/reactWebview/index.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-
-const vscode = acquireVsCodeApi();
-
-ReactDOM.render(<App vscode={vscode} />, document.getElementById("root"));
